26f46f5c0c8a85f92326da10d58def7cf347364b
felipesouza3083 <36085466+felipesouza3083@users.noreply.github.com>
2020-09-29 17:42:32 -0300

    Updates

---
+++ precache-manifest.4eeb58c3a4dd5e812623729cd7f79d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78d1cc5fa4266eefc5d621eba5048b5",
+    "url": "/projeto04/index.html"
+  },
+  {
+    "revision": "47782a3a021b388a8584",
+    "url": "/projeto04/static/js/2.ab4bcfc5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projeto04/static/js/2.ab4bcfc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ad16c596bb013e4e64",
+    "url": "/projeto04/static/js/main.9ecfd860.chunk.js"
+  },
+  {
+    "revision": "83ea9794016add774830",
+    "url": "/projeto04/static/js/runtime-main.debf446d.js"
+  }
+]);
